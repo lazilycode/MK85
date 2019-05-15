76beed5aee8c141c979acbba144f0906826f74c0
lazycode <wkl13022195532@gmail.com>
2019-05-15 20:07:36 +0800

    add code fullcalenar

--- vue-element-admin/src/views/documentation/index.vue
+++ vue-element-admin/src/views/documentation/index.vue
@@ -1,50 +1,108 @@
 <template>
-  <div class="app-container documentation-container">
-    <a class="document-btn" target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/">{{ $t('documentation.documentation') }}</a>
-    <a class="document-btn" target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">{{ $t('documentation.github') }}</a>
-    <a class="document-btn" target="_blank" href="https://panjiachen.gitee.io/vue-element-admin-site/zh/">国内文档</a>
-    <dropdown-menu :items="articleList" style="float:left;margin-left:50px;" title="系列文章" />
+  <div class="demo-app">
+    <div class="demo-app-top">
+      <button @click="toggleWeekends">toggle weekends</button>
+      <button @click="gotoPast">go to a date in the past</button>
+      (also, click a date/time to add an event)
+    </div>
+    <FullCalendar
+      ref="fullCalendar"
+      class="demo-app-calendar"
+      default-view="dayGridMonth"
+      :header="{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      }"
+      :locale="locale"
+      :plugins="calendarPlugins"
+      :weekends="calendarWeekends"
+      :events="events"
+      @dateClick="handleDateClick"
+      @eventMouseEnter="eventMouseEnter"
+    />
   </div>
 </template>
+
 <script>
-import DropdownMenu from '@/components/Share/DropdownMenu'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import zhcnLocale from '@fullcalendar/core/locales/zh-cn'
 
 export default {
-  name: 'Documentation',
-  components: { DropdownMenu },
-  data() {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data: function() {
     return {
-      articleList: [
-        { title: '基础篇', href: 'https://juejin.im/post/59097cd7a22b9d0065fb61d2' },
-        { title: '登录权限篇', href: 'https://juejin.im/post/591aa14f570c35006961acac' },
-        { title: '实战篇', href: 'https://juejin.im/post/593121aa0ce4630057f70d35' },
-        { title: 'vue-admin-template 篇', href: 'https://juejin.im/post/595b4d776fb9a06bbe7dba56' },
-        { title: 'v4.0 篇', href: 'https://juejin.im/post/5c92ff94f265da6128275a85' },
-        { title: '自行封装 component', href: 'https://segmentfault.com/a/1190000009090836' },
-        { title: '优雅的使用 icon', href: 'https://juejin.im/post/59bb864b5188257e7a427c09' },
-        { title: 'webpack4（上）', href: 'https://juejin.im/post/59bb864b5188257e7a427c09' },
-        { title: 'webpack4（下）', href: 'https://juejin.im/post/5b5d6d6f6fb9a04fea58aabc' }
+      calendarPlugins: [
+        // plugins must be defined in the JS
+        dayGridPlugin,
+        timeGridPlugin,
+        interactionPlugin // needed for dateClick
+      ],
+      locale: zhcnLocale,
+      calendarWeekends: true,
+      calendarEvents: [{ title: 'Event Now', start: new Date() }],
+      events: [
+        {
+          title: 'simple event',
+          start: '2019-05-02'
+        },
+        {
+          title: 'event with URL',
+          url: 'https://www.google.com/',
+          start: '2019-05-03'
+        }
       ]
+    }
+  },
+  methods: {
+    eventMouseEnter(e) {
+      console.log(e, 90)
+    },
+    toggleWeekends() {
+      this.calendarWeekends = !this.calendarWeekends // update a property
+    },
+    gotoPast() {
+      const calendarApi = this.$refs.fullCalendar.getApi() // from the ref="..."
+      calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+    },
+    handleDateClick(arg) {
+      console.log(arg)
+      // if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+      //   this.calendarEvents.push({
+      //     // add new event data
+      //     title: "New Event",
+      //     start: arg.date,
+      //     allDay: arg.allDay
+      //   })
+      // }
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.documentation-container {
-  margin: 50px;
-  .document-btn {
-    float: left;
-    margin-left: 50px;
-    display: block;
-    cursor: pointer;
-    background: black;
-    color: white;
-    height: 60px;
-    width: 200px;
-    line-height: 60px;
-    font-size: 20px;
-    text-align: center;
-  }
+<style lang='scss'>
+// you must include each plugins' css
+// paths prefixed with ~ signify node_modules
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
+@import "~@fullcalendar/timegrid/main.css";
+
+.demo-app {
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-top {
+  margin: 0 0 3em;
+}
+
+.demo-app-calendar {
+  margin: 0 auto;
+  max-width: 900px;
 }
 </style>
