344b9b05b569e62947652c3fdd5136ae77a90f2c
lazycode <wkl13022195532@gmail.com>
2019-06-09 20:57:08 +0800

    add code addDay 7

--- cnode-api/app/controller/user.js
+++ cnode-api/app/controller/user.js
@@ -2,8 +2,8 @@ const Controller = require('egg').Controller;
 class User extends Controller {
     async info() {
         const { ctx } = this;
-        const userInfo = await ctx.service.user.info();
-         ctx.body = userInfo;
+        const userInfo = await ctx.service.user.info(ctx.query);
+        ctx.body = userInfo;
     }
 }
 module.exports = User;
--- cnode-api/app/service/user.js
+++ cnode-api/app/service/user.js
@@ -1,11 +1,28 @@
 const Service = require('egg').Service;
 class User extends Service {
-    async info() {
+    async info(arg) {
         let arr=[]
         const { ctx } = this;
-        const user = await this.app.mysql.query("select * from data where start>'2012-01-20 00:00:00' and start<'2012-02-7 00:00:00';");
-        arr.push(user)
-        return arr;
+        let seventDay= this.AddDays(arg.startTime,7)
+        console.log(arg.startTime,seventDay,999)
+        //    2019-06-08 2019-06-15 999
+        const user = await this.app.mysql.query("select * from data where start>'"+arg.startTime+"' and start<'"+seventDay+"';")
+        return user;
     }
+
+    //addDay
+    AddDays(date, days) {
+        var nd = new Date(date)
+        nd = nd.valueOf()
+        nd = nd + days * 24 * 60 * 60 * 1000
+        nd = new Date(nd)
+        var y = nd.getFullYear()
+        var m = nd.getMonth() + 1
+        var d = nd.getDate()
+        if (m <= 9) m = '0' + m
+        if (d <= 9) d = '0' + d
+        var cdate = y + '-' + m + '-' + d
+        return cdate
+      }
 }
 module.exports = User;
