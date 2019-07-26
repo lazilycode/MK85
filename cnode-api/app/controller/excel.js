2afdcd767ca3f65e4ba1b117b417917fcc117fe3
lazycode <wkl13022195532@gmail.com>
2019-07-26 10:21:44 +0800

    add code 文件协同完毕

--- cnode-api/app/controller/excel.js
+++ cnode-api/app/controller/excel.js
@@ -15,32 +15,13 @@ class Excel extends Controller {
         //请求返回，生成的xlsx文件
         ctx.body = fs.readFileSync('output.xlsx');
         //请求返回后，删除生成的xlsx文件，不删除也行，下次请求回覆盖
-        // fs.rmdirSync('out.xlsx');
-        // deleteFolder('./')
-
-    }
-
-     deleteFolder(path) {
-        var files = [];
-        if (fs.existsSync(path)) {
-            if (fs.statSync(path).isDirectory()) {
-                files = fs.readdirSync(path);
-                files.forEach(function (file, index) {
-                    var curPath = path + "/" + file;
-                    if (fs.statSync(curPath).isDirectory()) {
-                        deleteFolder(curPath);
-                    } else {
-                        fs.unlinkSync(curPath);
-                    }
-                });
-                fs.rmdirSync(path);
-            } else {
-                fs.unlinkSync(path);
+        fs.unlink('output.xlsx', function(err) {
+            if (err) {
+                return console.error(err);
             }
-        }
+            console.log("文件删除成功！");
+         });
     }
-
-
 }
 
 module.exports = Excel;
