c48ddcdd60f6d9ed3d99235269216f3d711ac6a0
lazycode <wkl13022195532@gmail.com>
2019-08-01 07:40:01 +0800

    add code

--- vue-element-admin/src/views/icons/index.vue
+++ vue-element-admin/src/views/icons/index.vue
@@ -12,8 +12,20 @@
     {{ value3 }}
     <table id="customers">
       <tr>
-        <th> <span @click="fist"> ddd</span></th>
-        <th v-for=" (item,index) in headWeek" :key="index">
+        <th class="headr_row">
+          <!-- <span class="headCureent_text" @click="fist"> ddd</span> -->
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              常规时间<i class="el-icon-s-unfold el-icon--right" />
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item @click.native="changeTime(1,'常规时间')">常规时间</el-dropdown-item>
+              <el-dropdown-item @click.native="changeTime(2,'全部时间')">全部时间</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+
+        </th>
+        <th v-for=" (item,index) in headWeek" :key="index" class="headr_row">
           <div class="headCureent_text">
             <span :class="{'headCureent':item.current}">
               {{ item.day }}
@@ -23,25 +35,20 @@
 
         </th>
       </tr>
-      <tr v-for="(row,rowindex) in arr" :key="rowindex">
+      <tr v-for="(row,rowindex) in arr" :key="rowindex" :class="{ 'classRestTime': row.start=='12:00'||row.start=='17:50'}">
         <td>
           <div>
             {{ row.start }}-{{ row.end }}
           </div>
         </td>
+
         <td
-          v-for=" (item, index) in headWeek"
+          v-for=" (item, index) in row.chaild"
           :key="index"
-          @click="eventClick(row.start,item.date,$event)"
+          @click="eventClick(item,$event)"
         >
           <div v-for="(ite, ind) in daArr" :key="ind">
-            <!-- {{ row.start===ite.start&&item.date===ite.day }} -->
-            <a v-if="row.start===ite.start&&item.date===ite.day" slot="reference" :class="{ 'class-a': ite.className}" :style="{bottom:-ite.maio*30+'px'}"> {{ ite.title }}</a>
-            <!-- <el-popover placement="top-start" width="200" :trigger="ite.hover==='hover'?'hover':'manual'">
-              <div v-html="ite.html" />
-              {{ ite }}
-
-            </el-popover> -->
+            <a v-if="timeRange(row.start,row.end,ite.start)&&item.date===ite.day" slot="reference" :class="{ 'class-a': ite.className}" :style="{bottom:-ite.bottomHeight+'px',top:ite.topHeight+'px',backgroundColor: ite.Bg}" @click="markClick(ite,$event)"> {{ ite.title }}</a>
           </div>
         </td>
       </tr>
@@ -50,56 +57,61 @@
 </template>
 
 <script>
-import { formatDate, getMonDate, getDayName } from './uts'
+import { formatDate, getMonDate, getDayName, time_range } from './uts'
+// import { debounce } from '@/utils'
 export default {
-  data() {
+  data () {
     return {
       value3: '',
       nowDate: new Date(),
       arr: [],
       headWeek: [],
-      space: '30',
-      startTime: '08:00',
-      endTime: '18:00',
       config: {
-        minTime: '08:00',
-        maxTime: '18:00',
-        slotDuration: '30'
+        minTime: '07:00',
+        maxTime: '26:00',
+        slotDuration: '40'
       },
 
       daArr: [
+        // {
+        //   html: '<span>sdjcbniunciuejnce<span>',
+        //   startDay: '2019-07-30 11:00',
+        //   endDay: '2019-07-30 15:00',
+        //   day: '2019-07-30',
+        //   start: '11:00',
+        //   title: 35521253,
+        //   Bg:'red',
+        //   className: 'demo'
+        // },
         {
           html: '<span>sdjcbniunciuejnce<span>',
-          startDay: '2019-07-30 11:00',
-          endDay: '2019-07-31 15:00',
-          day: '2019-07-30',
-          start: '11:00',
-          title: 353,
+          startDay: '2019-07-31 11:45',
+          endDay: '2019-07-31 16:55',
+          title: 3552125312121,
+          Bg: '#74a8fb',
           className: 'demo'
         },
         {
           html: '<span>sdjcbniunciuejnce<span>',
-          startDay: '2019-07-30 11:30',
-          endDay: '2019-07-30 15:00',
-          day: '2019-07-30',
-          start: '11:30',
-          title: 353,
-          className: 'demo'
-        },
-        {
-          html: '<span>sdjcbniunciuejnce<span>',
-          hover: 'hover',
-          startDay: '2019-7-31 11:30',
-          endDay: '2019-7-31 12:00',
-          day: '2019-7-31',
-          start: '11:30',
-          title: '23e23e23e23e',
+          startDay: '2019-08-01 07:40',
+          endDay: '2019-08-01 08:20',
+          title: 35521253,
+          Bg: 'red',
           className: 'demo'
         }
+        // {
+        //   html: '<span>sdjcbniunciuejnce<span>',
+        //  startDay: '2019-07-30 11:00',
+        //   endDay: '2019-07-30 15:00',
+        //   day: '2019-07-30',
+        //   start: '11:30',
+        //   title: 353,
+        //   className: 'demo'
+        // }
       ]
     }
   },
-  mounted() {
+  mounted () {
     // 数据校验
     this.dataTr()
     this.headerTable(this.nowDate)
@@ -109,50 +121,56 @@ export default {
   },
   methods: {
 
-    count(b, a) {
+    count (b, a) {
+      // 每个高度为30px
       var date1 = new Date(b)
       var date2 = new Date(a)
-      var date = (date1.getTime() - date2.getTime()) / (1000 * 60 * parseInt(this.config.slotDuration))
-      return date
+      const topHeight = ((date2.getTime() / 1000 / 60) % parseInt(this.config.slotDuration)) / parseInt(this.config.slotDuration) * 30
+      const bottomHeight = (date1.getTime() - date2.getTime()) / (1000 * 60) / parseInt(this.config.slotDuration) * 30 - topHeight
+      return { bottomHeight, topHeight }
     },
 
-    dataTr() {
+    dataTr () {
+      if (!this.config.minTime && !this.config.maxTime && !this.config.slotDuration) {
+        this.config.minTime = '00:00'
+        this.config.maxTime = '26:00'
+        this.config.slotDuration = '30'
+      }
       this.daArr.map((item, index) => {
-        item.maio = this.count(item.endDay, item.startDay)
+        item.bottomHeight = this.count(item.endDay, item.startDay).bottomHeight
+        item.topHeight = this.count(item.endDay, item.startDay).topHeight
+        item.day = formatDate(item.startDay, 'YYYY-MM-DD')
+        item.start = formatDate(item.startDay, 'hh:mm')
       })
     },
 
-    sunDayWeek(e) {
+    sunDayWeek (e, r) {
       const arr = []
-      if (!this.startTime) {
-        this.startTime = '00:00'
-        this.endTime = '24:00'
-      }
-      const next = parseInt(this.endTime.slice(0, 2)) - parseInt(this.startTime.slice(0, 2))
-      const space = next * 60 * 60 / parseInt(this.space) / 60
-      for (let i = 0; i < space; i++) {
-        const timeStart =
-						(new Date(formatDate(e, 'YYYY-MM-DD') + ' ' + this.startTime).getTime() / 1000 +
-							i * parseInt(this.space) * 60) *
-						1000
-        const timeEnd =
-						(new Date(formatDate(e, 'YYYY-MM-DD') + ' ' + this.endTime).getTime() / 1000 +
-							(i + 1) * parseInt(this.space) * 60) *
-            1000
+      const next = (parseInt(this.config.maxTime.slice(0, 2)) - parseInt(this.config.minTime.slice(0, 2))) * 2
+      for (let i = 0; i < next; i++) {
+        let arrTime = []
+        const timeStart =	(new Date(formatDate(e, 'YYYY-MM-DD') + ' ' + this.config.minTime).getTime() / 1000 +	i * parseInt(this.config.slotDuration) * 60) * 1000
+        const timeEnd = (new Date(formatDate(e, 'YYYY-MM-DD') + ' ' + this.config.minTime).getTime() / 1000 +	(i + 1) * parseInt(this.config.slotDuration) * 60) *	1000
+        this.headWeek.map(item => {
+          let startTimes = (new Date(formatDate(item.date + ' ' + this.config.minTime, 'YYYY-MM-DD') + ' ' + this.config.minTime).getTime() / 1000 +	(i) * parseInt(this.config.slotDuration) * 60) * 1000
+          let endTimes = (new Date(formatDate(item.date + ' ' + this.config.minTime, 'YYYY-MM-DD') + ' ' + this.config.minTime).getTime() / 1000 +	(i + 1) * parseInt(this.config.slotDuration) * 60) * 1000
+          item.startTime = formatDate(new Date(startTimes), 'YYYY-MM-DD hh:mm')
+          item.endTime = formatDate(new Date(endTimes), 'YYYY-MM-DD hh:mm')
+          arrTime.push({ startTime: item.startTime, endTime: item.endTime, date: formatDate(new Date(item.startTime), 'YYYY-MM-DD') })
+        })
         arr.push({
+          chaild: arrTime,
           startDate: formatDate(new Date(timeStart), 'YYYY-MM-DD hh:mm'),
           endDate: formatDate(new Date(timeEnd), 'YYYY-MM-DD hh:mm'),
-          nowDay: formatDate(new Date(timeStart), 'YYYY-MM-DD'),
           start: formatDate(new Date(timeStart), 'hh:mm'),
           end: formatDate(new Date(timeEnd), 'hh:mm')
         })
       }
-      console.log(arr)
       this.arr = arr
     },
 
     // table 表格头部
-    headerTable(e) {
+    headerTable (e) {
       const d = getMonDate(e)
       const arr = []
       for (var i = 0; i < 7; i++) {
@@ -168,7 +186,7 @@ export default {
       this.headWeek = arr
     },
 
-    dataTreating() {
+    dataTreating () {
       // 初始化
       // const dataArr = []
       if (!this.config.minTime && !this.config.maxTime) {
@@ -178,7 +196,7 @@ export default {
     },
 
     // 较长的日期按时间段分割
-    timesection() {
+    timesection () {
       this.daArr.forEach((item, index) => {
         // 获取秒除以时间间隔
         let mistiming = new Date(item.endDay).getTime() - new Date(item.startDay).getTime()
@@ -187,43 +205,67 @@ export default {
       })
     },
 
-    eventClick(e, r, t) {
-      console.log(e, r, t)
+    eventClick (item, s) {
+      console.log(item, s)
     },
-    changeWeek() {
+    markClick (e, r) {
+      console.log(e, r, '5566')
+    },
+    changeWeek () {
       this.headerTable(this.value3)
     },
-    fist() {
-      this.sunDayWeek(this.nowDate)
-      this.startTime = '00:00'
-      this.endTime = '24:00'
+    changeTime (e, r) {
+      if (e == 1) {
+        this.$nextTick(() => {
+          this.sunDayWeek(this.nowDate, e)
+          // this.config.minTime= '07:00'
+          // this.config.maxTime= '24:00'
+          this.$set(this.config, 'minTime', '07:00')
+          this.$set(this.config, 'maxTime', '24:00')
+        })
+        // console.log( this.config)
+      } else {
+        this.$nextTick(() => {
+          this.sunDayWeek(this.nowDate, e)
+          // this.config.minTime = '00:20'
+          // this.config.maxTime= '24:00'
+          this.$set(this.config, 'minTime', '00:20')
+          this.$set(this.config, 'maxTime', '24:00')
+        })
+
+        // console.log( this.config)
+      }
+    },
+    timeRange (beginTime, endTime, nowTime) {
+      return time_range(beginTime, endTime, nowTime)
     }
+
   }
 }
 </script>
 <style scoped>
 #customers {
-	font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
 	width: 100%;
 	border-collapse: collapse;
+  overflow: hidden;
 }
 
 #customers td,
 #customers th {
 	text-align: center;
     font-size: 1em;
-    border: 1px solid #98bf21;
+    border: 1px solid #d5d9e1;
     position: relative;
     height: 30px;
 }
 
 #customers th {
 	text-align: center;
-	font-size: 1.1em;
+
 	text-align: left;
 	padding-top: 5px;
 	padding-bottom: 4px;
-	background-color: #a7c942;
+	background-color: #f5f5f5;
 	color: #ffffff;
 }
 
@@ -242,13 +284,24 @@ a.class-a {
 }
 
 .headCureent_text {
-  text-align: center;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+    color: gray;
 }
 
 span.headCureent {
-    width: 20px;
-    height: 20px;
-    border-radius: 16px;
-    background-color: red;
+  line-height: 25px;
+    width: 25px;
+    height: 25px;
+    border-radius: 17px;
+    background-color: #2c7eff;
+    display: inline-block;
+    color: #fff;
 }
+
+tr.classRestTime {
+    background-color: #e4e4e4;
+}
+
 </style>
